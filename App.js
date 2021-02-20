52f9c6b28c90c61e52b0935c0eb0a4b8cd7313f7
UttamBluLabs <uttam@blulabs.in>
2021-02-20 17:11:59 +0530

    Change port

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var fs = require("fs");
 
+const port = process.env.PORT || 8000;
+
 app.get("/listUsers", function (req, res) {
   console.log("🚀 ~ file: App.js ~ line 6 ~ req", req);
   fs.readFile(__dirname + "/" + "users.json", "utf8", function (err, data) {
@@ -14,7 +16,7 @@ app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-var server = app.listen(8081, function () {
+var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log("Example app listening at http://%s:%s", host, port);
